7ba093be529ef3f4f99dafd4d50de8ef0e358626
psm <a55026851@gmail.com>
2023-01-12 17:07:30 +0900

    test

---
+++ bicos/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>제발 되자 되어라 제발!!!!</h1>
+    </div>
+  );
+}
+
+export default App;
